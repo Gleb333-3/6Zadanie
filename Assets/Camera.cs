786b1a4275da25a3a353f1c4664c33fb1dede963
Gleb333-3 <glebefremenko23@gmail.com>
2021-03-23 21:59:24 +0300

    commit

---
+++ Assets/Camera.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Camera : MonoBehaviour
+{
+    internal static readonly object main;
+
+    internal static Vector3 ScreenToWorldPoint(Vector3 mousePosition)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Assets/brosok.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class brosok : MonoBehaviour
+{
+
+    public var play : boolean = true;
+public var speed: int = 10;
+public var moveDirection: Vector3;
+public var grounded: boolean;
+public var gravity: int = 1;
+        private void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            var dx: int = Input.mousePosition.x - Screen.width / 2.0;
+            var dy: int = Input.mousePosition.y - Screen.height / 2.0;
+            var strawRadians: float = Mathf.Atan2(dx, dy);
+            var strawDigrees:float = 360.0 * strawRadians / (2.0 * Mathf.PI);
+            transform.rotation.eulerAngles.y = strawDigrees;
+        }
+    }
+}
