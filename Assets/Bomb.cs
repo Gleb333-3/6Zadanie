e2113100a75f9faeb94a8917e203c373cd91fe3f
Gleb333-3 <glebefremenko23@gmail.com>
2021-03-23 23:11:57 +0300

    commit

---
+++ Assets/Bomb.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _Object;
+
+    [SerializeField]
+    private GameObject _WhatYdal;
+
+    [SerializeField]
+    private Transform _Bomb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var distance = Vector3.Distance(_Object.position, _Bomb.position);
+        float a = 1.4f;
+        if (distance < a) {
+
+            Destroy(_WhatYdal);
+        }
+    }
+}
